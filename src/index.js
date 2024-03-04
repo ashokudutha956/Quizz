e9d4041d010782fcbca4c99cf1be295a9dff7089
Manjula Udutha <ashokudutha956@gail.com>
2024-03-04 22:52:17 +0530

    ggg

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// index.js
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
