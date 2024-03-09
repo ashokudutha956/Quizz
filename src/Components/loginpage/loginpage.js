540c26be4a0fc0b31a05bab4ec168e8f3b64da29
Manjula Udutha <ashokudutha956@gail.com>
2024-03-09 15:52:48 +0530

    addeed

--- src/Components/loginpage/loginpage.js
+++ src/Components/loginpage/loginpage.js
@@ -3,40 +3,6 @@
 
 
 
-// import React, { useState } from 'react'
-// import {useNavigate } from 'react-router-dom';
-
-
-// const LoginPage=()=> {
-
-//     const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const navigate=useNavigate()
-
-
-//   const handleLogin=()=>{
-// navigate("/home")
-//   }
-//   return (
-//     <div className='ashok'>
-//       <h1>Login Page</h1>
-//       <label
-//       >
-//         Username:
-//         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-//       </label>
-//       <br />
-//       <label>
-//         Password:
-//         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-//       </label>
-//       <br />
-//       <button className='asn' onClick={handleLogin}>Login</button>
-//     </div>
-//   );
-// };
-
-// export default LoginPage
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
